633156ec2c8e85e1fa8e877d3cecee69d7a77d29
Andrew Wansik <amwansik@gmail.com>
2018-10-31 09:10:45 -0400

    updates made to user

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { UserService } from '../user.service';
+import { User } from '../user.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+
+  delete(): void {
+    this.usersvc.remove(this.user)
+      .subscribe(resp =>{
+        console.log("resp:", resp);
+        this.router.navigateByUrl('/users/list');
+      });
+  }
+
+  constructor(
+    private usersvc: UserService, 
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit() {
+    //gets the :id from the route
+    let id = this.route.snapshot.params.id;
+    //get user from user service
+    this.usersvc.get(id)
+      .subscribe(resp => {
+        console.log("resp", resp);
+        this.user = resp.data;
+      });
+
   }
 
 }
