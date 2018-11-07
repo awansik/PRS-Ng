d022ad8907d4d0f38c7721ad6e36a1f1944470b5
Andrew Wansik <amwansik@gmail.com>
2018-11-07 14:48:00 -0500

    updated menu

--- src/app/menu/menu/menu.component.ts
+++ src/app/menu/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Menu } from '../menu.class';
+import { SystemService } from '../../system/system.service';
 
 @Component({
   selector: 'app-menu',
@@ -9,16 +10,25 @@ import { Menu } from '../menu.class';
 })
 export class MenuComponent implements OnInit {
 
+  name: string;
+
   menus: Menu[] = [
     new Menu("Home", "/home", "The Home Page"),
     new Menu("Users", "/users/list", "The User List"),
     new Menu("Vendors", "/vendors/list", "The Vendor List"),
-    new Menu("About", "/about", "The About Page")
+    new Menu("Products", "/products/list", "The Products Page"),
+    new Menu("Requests", "/requests/list", "The Purchase Request List"),
+    new Menu("Review", '/requests/review-list', 'The Review Page'),
+    new Menu("About", "/about", "The About Page"),
+    new Menu("Login", "/users/login", "Login Page")
   ];
 
-  constructor() { }
+  constructor(
+    private sys: SystemService
+  ) { }
 
   ngOnInit() {
+    this.name = (this.sys.user != null) ? this.sys.user.lastName : 'Not logged in';
   }
 
 }
