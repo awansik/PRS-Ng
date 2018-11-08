b3212b12104d582534d1e7629cc603ac699c5d2e
Andrew Wansik <amwansik@gmail.com>
2018-11-08 09:42:09 -0500

    added SystemService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,46 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { UserListComponent } from './user/user-list/user-list.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserCreateComponent } from './user/user-create/user-create.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
+
+import { MenuComponent } from './menu/menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 import { VendorListComponent } from './vendor/vendor-list/vendor-list.component';
 import { VendorDetailComponent } from './vendor/vendor-detail/vendor-detail.component';
 import { VendorCreateComponent } from './vendor/vendor-create/vendor-create.component';
 import { VendorEditComponent } from './vendor/vendor-edit/vendor-edit.component';
 
-import { MenuComponent } from './menu/menu/menu.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { BoolDisplayPipe } from './pipes/bool-display.pipe';
 import { SortUsersPipe } from './pipes/sort-users.pipe';
+
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductCreateComponent } from './product/product-create/product-create.component';
+import { ProductEditComponent } from './product/product-edit/product-edit.component';
+
+import { UserLoginComponent } from './user/user-login/user-login.component';
+
+import { RequestListComponent } from './request/request-list/request-list.component';
+import { RequestDetailComponent } from './request/request-detail/request-detail.component';
+import { RequestCreateComponent } from './request/request-create/request-create.component';
+import { RequestEditComponent } from './request/request-edit/request-edit.component';
+import { RequestLinesComponent } from './request/request-lines/request-lines.component';
+
+import { SortPipe } from './pipes/sort.pipe';
+
+// import { RequestReviewDetailComponent } './request/request-review-detail/request-review-detail.component';
+// import { RequestReviewListComponent } './request/request-review-list/request-review-list.component';
+
+import { ReqlineDetailComponent } from './reqlines/reqline-detail/reqline-detail.component';
+import { ReqlineEditComponent } from './reqlines/reqline-edit/reqline-edit.component';
+import { ReqlineCreateComponent } from './reqlines/reqline-create/reqline-create.component';
+
 
 @NgModule({
   declarations: [
@@ -30,16 +55,38 @@ import { SortUsersPipe } from './pipes/sort-users.pipe';
     UserCreateComponent,
     UserEditComponent,
 
+    MenuComponent,
+    HomeComponent,
+    AboutComponent,
+
     VendorListComponent,
     VendorDetailComponent,
     VendorCreateComponent,
     VendorEditComponent,
     
-    MenuComponent,
-    HomeComponent,
-    AboutComponent,
     BoolDisplayPipe,
-    SortUsersPipe
+    SortUsersPipe,
+
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductCreateComponent,
+    ProductEditComponent,
+
+    RequestListComponent,
+    RequestDetailComponent,
+    RequestCreateComponent,
+    RequestEditComponent,
+    RequestLinesComponent,
+    // RequestReviewDetailComponent,
+    // RequestReviewListComponent,
+
+    ReqlineCreateComponent,
+    ReqlineDetailComponent,
+    ReqlineEditComponent,
+    
+    UserLoginComponent,
+
+    SortPipe
   ],
   imports: [
     BrowserModule,
